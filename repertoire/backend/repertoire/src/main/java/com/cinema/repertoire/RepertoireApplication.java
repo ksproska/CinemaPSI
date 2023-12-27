02f51e507c8b308aaed7be49935a4e1a19ab0590
Kamila Sproska <kamila.sproska@gmail.com>
2023-12-27 23:11:46 +0100

    RepertoireApplication init

---
+++ repertoire/backend/repertoire/src/main/java/com/cinema/repertoire/RepertoireApplication.java
@@ -0,0 +1,13 @@
+package com.cinema.repertoire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepertoireApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepertoireApplication.class, args);
+	}
+
+}
---
+++ repertoire/backend/repertoire/src/test/java/com/cinema/repertoire/RepertoireApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinema.repertoire;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepertoireApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
