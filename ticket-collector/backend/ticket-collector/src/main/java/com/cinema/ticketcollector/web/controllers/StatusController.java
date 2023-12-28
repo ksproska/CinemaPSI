e026dc23455133e718dde9b71a301c84a199bc97
Kamila Sproska <kamila.sproska@gmail.com>
2023-12-28 14:34:03 +0100

    added collect ticket endpoint with mocked data

--- ticket-collector/backend/ticket-collector/src/main/java/com/cinema/ticketcollector/web/controllers/StatusController.java
+++ ticket-collector/backend/ticket-collector/src/main/java/com/cinema/ticketcollector/web/controllers/StatusController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
@@ -25,10 +26,28 @@ public class StatusController {
     public ResponseEntity getTicketForId(@RequestBody TicketRequest ticketRequest) {
         LOG.info("Requested ticket with id {}", ticketRequest.ticketId());
         // TODO: Connect to database
-        var tempResponse = tempTicketsDb.stream().filter(t -> Objects.equals(t.id(), ticketRequest.ticketId())).findFirst();
+        var tempResponse = tempTicketsDb.stream().filter(t -> Objects.equals(t.getId(), ticketRequest.ticketId())).findFirst();
         if (tempResponse.isPresent()) {
             return ResponseEntity.ok(tempResponse.get());
         }
         return ResponseEntity.badRequest().body(String.format("Ticket with requested id %d not found.", ticketRequest.ticketId()));
+    }
+
+    @PostMapping("/collect")
+    public ResponseEntity collectTicketWithId(@RequestBody TicketRequest ticketRequest) {
+        LOG.info("Requested ticket with id {}", ticketRequest.ticketId());
+        // TODO: Connect to database
+        var tempResponse = tempTicketsDb
+                .stream()
+                .filter(t -> Objects.equals(t.getId(), ticketRequest.ticketId()))
+                .findFirst();
+        if (tempResponse.isEmpty()) {
+            return ResponseEntity.badRequest().body(String.format("Ticket with requested id %d not found.", ticketRequest.ticketId()));
+        }
+        if (!tempResponse.get().isValid()) {
+            return ResponseEntity.badRequest().body(String.format("Ticket with requested id %d is not valid.", ticketRequest.ticketId()));
+        }
+        tempResponse.get().setNotValid();
+        return ResponseEntity.ok(String.format("Ticket with requested id %d is collected successfully.", ticketRequest.ticketId()));
     }
 }
