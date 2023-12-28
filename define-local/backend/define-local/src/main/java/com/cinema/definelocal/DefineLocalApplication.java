30de67bbdba5c7231da1a759d1efe3eb37c25df7
Kamila Sproska <kamila.sproska@gmail.com>
2023-12-28 17:40:13 +0100

    define local init

---
+++ define-local/backend/define-local/src/main/java/com/cinema/definelocal/DefineLocalApplication.java
@@ -0,0 +1,13 @@
+package com.cinema.definelocal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefineLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefineLocalApplication.class, args);
+	}
+
+}
---
+++ define-local/backend/define-local/src/test/java/com/cinema/definelocal/DefineLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinema.definelocal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DefineLocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
