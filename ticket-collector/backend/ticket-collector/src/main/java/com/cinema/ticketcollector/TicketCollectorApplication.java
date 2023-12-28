f1c648391004449d5702af00fb1aaec36e368fce
Kamila Sproska <kamila.sproska@gmail.com>
2023-12-28 13:53:53 +0100

    ticket-collector init

---
+++ ticket-collector/backend/ticket-collector/src/main/java/com/cinema/ticketcollector/TicketCollectorApplication.java
@@ -0,0 +1,13 @@
+package com.cinema.ticketcollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketCollectorApplication.class, args);
+	}
+
+}
---
+++ ticket-collector/backend/ticket-collector/src/test/java/com/cinema/ticketcollector/TicketCollectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinema.ticketcollector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketCollectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
