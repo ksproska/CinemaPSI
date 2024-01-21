97916e90d1f0ab196b3586d23cbe8c1c4c7d6f3f
Kamila Sproska <kamila.sproska@gmail.com>
2024-01-21 14:57:12 +0100

    HealthCheckControllers added for each service

---
+++ client-service/backend/client-service/src/main/java/com/cinema/clientservice/web/controllers/HealthCheckController.java
@@ -0,0 +1,13 @@
+package com.cinema.clientservice.web.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HealthCheckController {
+    @GetMapping("/healthcheck")
+    public ResponseEntity<String> healthcheck() {
+        return ResponseEntity.ok("Client-service healthy");
+    }
+}
